84bb9d83796ad2a382165e107c22245403b22904
Diogo Cezar Teixeira Batista <diogo.batista@meifacil.com>
2021-01-23 11:29:49 -0300

    chore: first tests

--- index.js
+++ index.js
@@ -21,20 +21,39 @@ const sendMessage = (number, message, client) => {
   });
 }
 
+const creatLogs = (client) => {
+  console.log('CRIANDO LOGS')
+  client.onMessage((message) => {
+    console.log('MENSAGEM RECEBIDA')
+    console.log(message)
+    if (message.body === 'oi') {
+      client
+        .sendText(message.from, 'Abacate')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
+
 const createJob = (client) => {
   console.log('CRIANDO JOB')
   const job = new CronJob('*/15 * * * * *', function() {
     console.log('EXECUTANDO JOB')
-    sendMessage('554399813475@c.us', `Teste do Robô do Diogo. Agora são: ${new Date().toLocaleTimeString()}`, client)
-    sendMessage('G9SIDtm2VtGAiX34QrWJ12@g.us', `Teste do Robô do Diogo. Agora são: ${new Date().toLocaleTimeString()}`, client)
+    //sendMessage('554399813475@c.us', `Teste do Robô do Diogo. Agora são: ${new Date().toLocaleTimeString()}`, client)
+    sendMessage('5543933000663-1611408684@g.us', `Teste do Robô do Diogo. Agora são: ${new Date().toLocaleTimeString()}`, client)
   }, null, true, 'America/Sao_Paulo');
   return job;
 }
 
 const run = async () => {
   const client = await createVenomClient();
-  const job = createJob(client)
-  job.start()
+  creatLogs(client)
+  //const job = createJob(client)
+  //job.start()
 }
 
 run();
